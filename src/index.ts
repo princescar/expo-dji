a09168bc6b025ea8d2179dc7b9af461b19612ff7
Halbert <princescar@gmail.com>
2025-05-30 15:36:38 +0800

    remove ios and web

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-// Reexport the native module. On web, it will be resolved to ExpoDjiModule.web.ts
-// and on native platforms to ExpoDjiModule.ts
 export { default } from './ExpoDjiModule';
 export { default as ExpoDjiView } from './ExpoDjiView';
 export * from  './ExpoDji.types';
